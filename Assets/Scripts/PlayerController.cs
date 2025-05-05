aa423be19bd8995d75348e8bd10be99d8534b3e1
ThanapornA <thanaporn.apir@bumail.net>
2025-05-05 16:06:54 +0700

    Update#2
    - Player Controller

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,11 +1,18 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-
 public class PlayerController : MonoBehaviour
 {
+    ///Controller///
     private InputAction moveAction;
     private Rigidbody rb;
+    public float speed;
+    public float turnSpeed;
+
+    ///PickUp System///
+
+    ///Scoring///
+    public int score = 0;
 
     void Start()
     {
@@ -16,7 +23,23 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         float horiAction = moveAction.ReadValue<Vector2>().x;
-        float verticalAction = moveAction.ReadValue<Vector2>().y;
-        rb.AddForce( verticalAction * transform.right * speed , ForceMode.Force);
+        float vertiAction = moveAction.ReadValue<Vector2>().y;
+        transform.Translate( vertiAction * speed * Time.deltaTime * Vector3.forward);
+        transform.Translate( horiAction * speed * Time.deltaTime * Vector3.right);
+
+        if ( horiAction != 0 )
+        {
+            transform.Rotate( Vector3.up , turnSpeed * horiAction );
+        }
+
+        if ( Input.GetKeyDown(KeyCode.LeftShift) )
+        {
+            Debug.Log("You press SHIFT. now the speed = 9");
+            speed *= 3;
+        }
+        else if ( Input.GetKeyUp(KeyCode.LeftShift) )
+        {
+            speed = 3;
+        }
     }
 }
